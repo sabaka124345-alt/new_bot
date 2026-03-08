d2cc273a6473632144cb14bbb97210f86eabc0b0
sabaka124345-alt <sabaka124345@gmail.com>
2026-03-09 02:24:15 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
+console.log("APP START");
 
+const tg = window.Telegram.WebApp;
+
+tg.ready();
+tg.expand();
+
+document.getElementById("main").innerHTML = `
+<h1>Market started</h1>
+<p>Telegram user loaded</p>
+`;
