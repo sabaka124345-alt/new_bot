0b64aa7675eba78f76f36b06b91058cdae400f48
sabaka124345-alt <sabaka124345@gmail.com>
2026-03-09 00:02:03 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
+const tg = window.Telegram.WebApp;
 
+tg.expand();
+
+console.log("Mini App started");
+
+const user = tg.initDataUnsafe.user;
+
+if(user){
+console.log("User ID:", user.id);
+console.log("Username:", user.username);
+}
