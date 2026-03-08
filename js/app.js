a5eb34595e0a2e398183761aa379d2a622fcf7d3
sabaka124345-alt <sabaka124345@gmail.com>
2026-03-08 23:14:52 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -20,3 +20,73 @@ if (!tg || !tg.initData) {
 tg.expand();
 
 console.log("Telegram WebApp initialized");
+const app = document.getElementById("app");
+
+function renderHome() {
+
+    const categories = [
+        "Жидкости",
+        "POD-системы",
+        "Съедобные товары",
+        "Расслабляющий чай",
+        "Съедобные грибы",
+        "Семена конопли"
+    ];
+
+    let html = `<div class="categories">`;
+
+    categories.forEach(cat => {
+        html += `
+    <div class="category-card" onclick="openCategory('${cat}')">
+    ${cat}
+</div>
+`;
+});
+
+    html += `</div>`;
+
+    app.innerHTML = html;
+}
+
+renderHome();
+
+function openCategory(category){
+
+const items = PRODUCTS.filter(p => p.category === category);
+
+let html = `
+<div style="padding:20px">
+
+<h2>${category}</h2>
+
+<div class="products">
+`;
+
+items.forEach(p => {
+
+html += `
+<div class="product-card">
+
+<div class="product-title">${p.name}</div>
+
+<div class="product-price">${p.price} ₽</div>
+
+<button onclick="addToCart(${p.id})">
+Добавить в корзину
+</button>
+
+</div>
+`;
+
+});
+
+html += `</div></div>`;
+
+app.innerHTML = html;
+
+}
+function addToCart(id){
+
+console.log("Added product:", id);
+
+}
