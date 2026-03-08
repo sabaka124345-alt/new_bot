61145f6165bcaa126fb15578f0399ca9bf54bb3c
sabaka124345-alt <sabaka124345@gmail.com>
2026-03-09 02:28:52 +0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,11 +1,67 @@
-console.log("APP START");
-
 const tg = window.Telegram.WebApp;
 
 tg.ready();
 tg.expand();
 
+let cities = [
+"Москва",
+"Санкт-Петербург",
+"Казань",
+"Екатеринбург",
+"Новосибирск",
+"Краснодар",
+"Ростов-на-Дону",
+"Новосибирск",
+"Омск",
+"Самара",
+"Челябинск"
+];
+
+let selectedCity = null;
+
+function renderCities(list){
+
+const container = document.getElementById("cityList");
+container.innerHTML = "";
+
+list.forEach(city => {
+
+const div = document.createElement("div");
+div.innerText = city;
+
+div.onclick = () => {
+
+selectedCity = city;
+
+document.querySelectorAll("#cityList div").forEach(el=>{
+el.style.background = "";
+});
+
+div.style.background = "#333";
+
+};
+
+container.appendChild(div);
+
+});
+
+}
+
+renderCities(cities);
+
+function enterMarket(){
+
+if(!selectedCity){
+alert("Выберите город");
+return;
+}
+
+document.getElementById("cityGate").style.display="none";
+document.getElementById("main").style.display="block";
+
 document.getElementById("main").innerHTML = `
-<h1>Market started</h1>
-<p>Telegram user loaded</p>
+<h1>Город: ${selectedCity}</h1>
+<p>Маркет загружен</p>
 `;
+
+}
